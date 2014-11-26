76260f839c241d2c8154ac368dcaab74b7ad540a
dvberkel <daan.v.berkel.1980@gmail.com>
2014-11-26 07:41:51 +0100

    extract reusable code

--- js/walkthrough.js
+++ js/walkthrough.js
@@ -1,40 +1,43 @@
 /* global window, document, Reveal*/
 ;(function(document, Reveal, Maze, MazeView){
 	'use strict';
-	var isSetup = false;
+	var maze = new Maze();
+	maze.addObstruction(-1, -1);
+	maze.addObstruction(0, -1);
+	maze.addObstruction(1, -1);
+	maze.addObstruction(2, -1);
+	maze.addObstruction(3, -1);
+	maze.addObstruction(4, -1);
+	maze.addObstruction(4, 0);
+	maze.addObstruction(4, 1);
+	maze.addObstruction(4, 2);
+	maze.addObstruction(4, 3);
+	maze.addObstruction(3, 3);
+	maze.addObstruction(2, 3);
+	maze.addObstruction(1, 3);
+	maze.addObstruction(0, 3);
+	maze.addObstruction(-1, 3);
+	maze.addObstruction(-1, 2);
+	maze.addObstruction(-1, 1);
+	maze.addObstruction(-1, 0);
+
+	maze.addObstruction(1, 1);
+
+	maze.addGoal(3, 2);
+	maze.addGoal(3, 1, -1);
+
+	var sizeMultiplier = 8;
+
+	var isSetup = {
+		'walkthrough': false
+	};
 	Reveal.addEventListener('walkthrough', function(){
-		if (!isSetup) {
-			isSetup = true;
+		if (!isSetup.walkthrough) {
+			isSetup.walkthrough = true;
 			var container = document.getElementById('walkthrough-maze');
 
-			var maze = new Maze();
-			maze.addObstruction(-1, -1);
-			maze.addObstruction(0, -1);
-			maze.addObstruction(1, -1);
-			maze.addObstruction(2, -1);
-			maze.addObstruction(3, -1);
-			maze.addObstruction(4, -1);
-			maze.addObstruction(4, 0);
-			maze.addObstruction(4, 1);
-			maze.addObstruction(4, 2);
-			maze.addObstruction(4, 3);
-			maze.addObstruction(3, 3);
-			maze.addObstruction(2, 3);
-			maze.addObstruction(1, 3);
-			maze.addObstruction(0, 3);
-			maze.addObstruction(-1, 3);
-			maze.addObstruction(-1, 2);
-			maze.addObstruction(-1, 1);
-			maze.addObstruction(-1, 0);
-
-			maze.addObstruction(1, 1);
-
-			maze.addGoal(3, 2);
-			maze.addGoal(3, 1, -1);
-
-			var n = 8;
 			new MazeView(maze, container, {
-				width: n * 60, height: n * 50,
+				width: sizeMultiplier * 60, height: sizeMultiplier * 50,
 				rewardColor: { positive: 'green', negative: 'red' }
 			});
 		}
