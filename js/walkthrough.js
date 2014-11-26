bc2048433be9d939e48c6bc8005d4a396d35918b
dvberkel <daan.v.berkel.1980@gmail.com>
2014-11-26 22:02:17 +0100

    evaluate maze with valuation functions

--- js/walkthrough.js
+++ js/walkthrough.js
@@ -31,6 +31,7 @@
 		'walkthrough2': false,
 		'walkthrough3': false,
 		'walkthrough4': false,
+		'walkthrough6': false,
 		'walkthrough5': false
 	};
 	Reveal.addEventListener('walkthrough-1', function(){
@@ -197,6 +198,48 @@
 				{ x: 1 , y: 2, value: '←'},
 				{ x: 2 , y: 2, value: '←'},
 				{ x: 3 , y: 2, value: '←'},
+			].forEach(function(data){
+				console.log(data);
+				ctx.fillText(
+					data.value,
+					dx * (data.x - mx + 0.5),
+					dy * (data.y - my + 0.5)
+				);
+			});
+		}
+	});
+	Reveal.addEventListener('walkthrough-6', function(){
+		if (!isSetup.walkthrough6) {
+			isSetup.walkthrough6 = true;
+			var container = document.getElementById('walkthrough-6-maze');
+
+			var view = new MazeView(maze, container, {
+				width: 480, height: 400,
+				rewardColor: { positive: 'green', negative: 'red' }
+			});
+
+			var ctx = view.context();
+			var dx = view.options.dx;
+			var dy = view.options.dy;
+			var mx = view.options.mx;
+			var my = view.options.my;
+
+			ctx.fillStyle = 'blue';
+			ctx.font = '15px sans-serif';
+			ctx.textAlign = 'center';
+			ctx.textBaseline = 'middle';
+			[
+				{ x: 0 , y: 0, value: 0.5},
+				{ x: 1 , y: 0, value: 1.0},
+				{ x: 2 , y: 0, value: 1.5},
+				{ x: 3 , y: 0, value: 2},
+				{ x: 0 , y: 1, value: 0.3},
+				{ x: 2 , y: 1, value: 1.3},
+				{ x: 3 , y: 1, value: 1.8},
+				{ x: 0 , y: 2, value: 0.1},
+				{ x: 1 , y: 2, value: 0.6},
+				{ x: 2 , y: 2, value: 1.1},
+				{ x: 3 , y: 2, value: 1.6},
 			].forEach(function(data){
 				console.log(data);
 				ctx.fillText(
